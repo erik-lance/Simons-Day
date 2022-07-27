cb05e65bf78d7f3bd192d1122fe9f7731a01ca66
erik-lance <eriklance@gmail.com>
2022-07-28 00:14:00 +0800

    Added broken cell, added walking bool, added actors adder for each cell

--- assets/scripts/cell_manager.gd
+++ assets/scripts/cell_manager.gd
@@ -4,6 +4,7 @@ var class_cell = "res://scenes/cells/classroom_cell.tscn"
 var locker_cell_1 = "res://scenes/cells/locker_cell.tscn"
 var locker_cell_2 = "res://scenes/cells/locker_cell_2.tscn"
 var window_cell = "res://scenes/cells/window_cell.tscn"
+var broken_cell = "res://scenes/cells/broken_cell.tscn"
 
 onready var cell = $Cells
 onready var cell_1 = $"Cells/Cell 1"
@@ -13,7 +14,7 @@ onready var cell_3 = $"Cells/Cell 3"
 var cast_unknown = "res://scenes/actors/unknown.tscn"
 
 var speed = 30
-var walking = true
+export (bool) var walking = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -40,13 +41,14 @@ func _physics_process(delta):
 		
 
 func rand_cell():
-	var n = randi() % 4
+	var n = randi() % 5
 	
 	match(n):
 		0: return class_cell
 		1: return locker_cell_1
 		2: return locker_cell_2
 		3: return window_cell
+		4: return broken_cell
 	
 
 # Loads cell at cell n with type c
@@ -61,10 +63,39 @@ func load_cell(n,c):
 		0: cell_1.add_child(loaded_cell)
 		1: cell_2.add_child(loaded_cell)
 		2: cell_3.add_child(loaded_cell)
+	
+	add_actors(loaded_cell)
 
-func load_unknown(s=0):
+func add_actors(cell):
+	var n = randi() % 5
+	
+	# Maximum of 5 actors
+	for i in n:
+		var unk_actor = load_unknown(cell)
+		var x = randi() % 224 + 9
+		# from 9 to 233 inclusive
+		
+		var y = randi() % 10
+		y += 40
+		
+		unk_actor.position.x = x
+		unk_actor.position.y = y
+
+func load_unknown(cell):
 	var loaded_cast = load(cast_unknown).instance()
+	cell.add_child(loaded_cast)
 	
+	var n = randi() % 2
+	match(n):
+		0: loaded_cast.get_child(0).animation = 'idle'
+		1: loaded_cast.get_child(0).animation = 'talk'
 	
-	pass
+	n = randi() % 2
+	match(n):
+		0: loaded_cast.get_child(0).flip_h = false
+		1: loaded_cast.get_child(0).flip_h = true
+	
+	return loaded_cast
 
+func set_walking(t):
+	walking = t
