a81b33791a0dcda274e35aea457edc4a9498f4dc
erik-lance <eriklance@gmail.com>
2022-07-28 00:19:38 +0800

    Adjusted slider based on AudioBus db

--- assets/scripts/settings.gd
+++ assets/scripts/settings.gd
@@ -10,23 +10,14 @@ func _ready():
 	music_slider.value = AudioServer.get_bus_volume_db(1)
 	SFX_slider.value = AudioServer.get_bus_volume_db(2)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_Master_value_changed(value):
 	AudioServer.set_bus_volume_db(0, value)
-
 
 func _on_Music_value_changed(value):
 	AudioServer.set_bus_volume_db(1, value)
 
-
 func _on_SFX_value_changed(value):
 	AudioServer.set_bus_volume_db(2, value)
-
 
 func _on_Button_button_up():
 	get_tree().change_scene_to(load("res://scenes/main_menu.tscn"))
