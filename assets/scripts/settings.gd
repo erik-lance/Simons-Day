8d665ff9cce4af0ef6313040db2cb20b133b76e4
erik-lance <eriklance@gmail.com>
2022-07-28 17:59:07 +0800

    Cleaned name, added audio for moving SFX so player knows

--- assets/scripts/settings.gd
+++ assets/scripts/settings.gd
@@ -2,9 +2,9 @@ extends Node2D
 
 signal btn(s)
 
-onready var master_slider = $Menu/ColorRect/VBoxContainer/Master
-onready var music_slider=  $Menu/ColorRect/VBoxContainer/Music
-onready var SFX_slider = $Menu/ColorRect/VBoxContainer/SFX
+onready var master_slider = $SettingsMenu/ColorRect/VBoxContainer/Master
+onready var music_slider=  $SettingsMenu/ColorRect/VBoxContainer/Music
+onready var SFX_slider = $SettingsMenu/ColorRect/VBoxContainer/SFX
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,6 +19,8 @@ func _on_Music_value_changed(value):
 	AudioServer.set_bus_volume_db(1, value)
 
 func _on_SFX_value_changed(value):
+	if !$SettingsMenu/AudioStreamPlayer.playing:
+		$SettingsMenu/AudioStreamPlayer.play()
 	AudioServer.set_bus_volume_db(2, value)
 
 func _on_Button_button_up():
