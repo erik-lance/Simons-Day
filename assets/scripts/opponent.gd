72fb4e050f16644a0c208a222604c34d59ce378b
erik-lance <eriklance@gmail.com>
2022-07-30 14:47:42 +0800

    Added target grabber and damage signal parameter

--- assets/scripts/opponent.gd
+++ assets/scripts/opponent.gd
@@ -1,15 +1,22 @@
 extends Node2D
 
 signal defeated
-signal damage
+signal damage(d)
 
 var max_hp
 var hp
 
+var cur_target = null
+
+enum challenger {UNKNOWN, LAURA, FLYNN}
+var challenge_type =  challenger.UNKNOWN
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
+func get_target() : return cur_target
+func set_target(t): cur_target = t
 
 func set_new_hp(h):
 	max_hp = h
@@ -23,4 +30,13 @@ func take_hit(d):
 	if hp <= 0:
 		emit_signal('defeated')
 	else:
-		emit_signal('damage')
+		emit_signal('damage',d)
+
+
+func _on_Timer_timeout():
+	if challenge_type == challenger.FLYNN:
+		pass
+	elif challenge_type == challenger.LAURA:
+		pass
+	else:
+		pass
